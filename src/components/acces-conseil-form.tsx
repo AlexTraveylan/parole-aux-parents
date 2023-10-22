b06fa24c269beb6ce3a04af59e38decdb2188bc0
AlexTraveylan <monsieurdemares@gmail.com>
2023-10-22 23:42:11 +0200

    connexion requise

--- src/components/acces-conseil-form.tsx
+++ src/components/acces-conseil-form.tsx
@@ -52,7 +52,7 @@ export function AccesConseilFrom({ setIsCreate }: { setIsCreate: (value: boolean
       <CardHeader>
         <CardTitle>Un espace est déjà crée ?</CardTitle>
         <CardDescription>
-          Accède à cette espace, lis les questions des autres parents, fait des commentaires, ajoute tes propres questions.
+          Accède à cette espace, lis les questions des autres parents, fait des commentaires, ajoute tes propres questions. Connexion requise.
         </CardDescription>
       </CardHeader>
       <CardContent>
