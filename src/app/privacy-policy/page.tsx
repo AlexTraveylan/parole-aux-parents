727ad2d41bae1456c961543caa8c2dd19595928e
AlexTraveylan <monsieurdemares@gmail.com>
2023-11-18 22:15:55 +0100

    stringify

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,54 +1,58 @@
 export default async function PrivacyPolicy() {
   return (
     <div>
-      <h1>Politique de confidentialité</h1>
-      <p>Bienvenue sur l'application "Parole aux parents" !</p>
+      <h1>{"Politique de confidentialité"}</h1>
+      <p>{"Bienvenue sur l'application 'Parole aux parents' !"}</p>
       <p>
-        Nous accordons une grande importance à la protection de vos données personnelles. La présente politique de confidentialité vise à vous
-        informer sur les informations que nous recueillons, comment nous les utilisons et comment nous les protégeons.
+        {
+          "Nous accordons une grande importance à la protection de vos données personnelles. La présente politique de confidentialité vise à vous informer sur les informations que nous recueillons, comment nous les utilisons et comment nous les protégeons."
+        }
       </p>
-      <h2>Collecte des informations</h2>
-      <p>Lorsque vous utilisez l'application "Parole aux parents", nous pouvons recueillir les données suivantes :</p>
+      <h2>{"Collecte des informations"}</h2>
+      <p>{"Lorsque vous utilisez l'application 'Parole aux parents', nous pouvons recueillir les données suivantes :"}</p>
       <ul>
-        <li>Informations d'identification (nom, adresse e-mail, etc.) fournies lors de la création de votre compte.</li>
+        <li>{"Informations d'identification (nom, adresse e-mail, etc.) fournies lors de la création de votre compte."}</li>
         <li>
-          Informations relatives à votre utilisation de l'application, telles que les questions que vous posez, les commentaires que vous faites et
-          les contenus que vous likez.
+          {
+            "Informations relatives à votre utilisation de l'application, telles que les questions que vous posez, les commentaires que vous faites et les contenus que vous likez."
+          }
         </li>
       </ul>
-      <h2>Utilisation des informations</h2>
-      <p>Les informations que nous recueillons sont utilisées dans les buts suivants :</p>
+      <h2>U{"tilisation des informations"}</h2>
+      <p>L{"es informations que nous recueillons sont utilisées dans les buts suivants :"}</p>
       <ul>
-        <li>Permettre le bon fonctionnement de l'application "Parole aux parents".</li>
-        <li>Améliorer et optimiser notre application ainsi que votre expérience utilisateur.</li>
-        <li>Répondre à vos questions et demandes de support.</li>
+        <li>{"Permettre le bon fonctionnement de l'application 'Parole aux parents'."}</li>
+        <li>{"Améliorer et optimiser notre application ainsi que votre expérience utilisateur."}</li>
+        <li>{"Répondre à vos questions et demandes de support."}</li>
       </ul>
-      <h2>Protection des informations</h2>
+      <h2>{"Protection des informations"}</h2>
       <p>
-        Nous mettons en œuvre des mesures de sécurité pour protéger vos informations personnelles contre tout accès non autorisé ou toute perte
-        accidentelle. Toutefois, veuillez noter qu'aucune méthode de transmission sur Internet ou de stockage électronique n'est totalement
-        sécurisée. Nous ne pouvons donc pas garantir une sécurité absolue des informations.
+        {
+          "Nous mettons en œuvre des mesures de sécurité pour protéger vos informations personnelles contre tout accès non autorisé ou toute perte accidentelle. Toutefois, veuillez noter qu'aucune méthode de transmission sur Internet ou de stockage électronique n'est totalement sécurisée. Nous ne pouvons donc pas garantir une sécurité absolue des informations."
+        }
       </p>
-      <h2>Partage des informations</h2>
+      <h2>{"Partage des informations"}</h2>
       <p>
-        Nous ne vendons, n'échangeons ni ne louons vos informations personnelles à des tiers sans votre consentement. Cependant, nous pouvons
-        partager vos informations avec des prestataires de services qui nous aident à exploiter notre application ou à fournir des services
-        connexes, sous réserve d'accords de confidentialité appropriés.
+        {
+          "Nous ne vendons, n'échangeons ni ne louons vos informations personnelles à des tiers sans votre consentement. Cependant, nous pouvons partager vos informations avec des prestataires de services qui nous aident à exploiter notre application ou à fournir des services connexes, sous réserve d'accords de confidentialité appropriés."
+        }
       </p>
-      <h2>Conservation des informations</h2>
+      <h2>{"Conservation des informations"}</h2>
       <p>
-        Nous ne conserverons vos informations personnelles que pendant la durée nécessaire pour atteindre les objectifs énoncés dans la présente
-        politique de confidentialité, sauf si une conservation plus longue est requise ou autorisée par la loi.
+        {
+          "Nous ne conserverons vos informations personnelles que pendant la durée nécessaire pour atteindre les objectifs énoncés dans la présente politique de confidentialité, sauf si une conservation plus longue est requise ou autorisée par la loi."
+        }
       </p>
-      <h2>Vos droits</h2>
+      <h2>{"Vos droits"}</h2>
       <p>
-        Vous avez le droit de demander l'accès, la rectification ou la suppression de vos informations personnelles. Si vous souhaitez exercer ces
-        droits ou si vous avez des questions concernant notre politique de confidentialité, veuillez nous contacter à l'adresse
-        privacy@example.com.
+        {
+          "Vous avez le droit de demander l'accès, la rectification ou la suppression de vos informations personnelles. Si vous souhaitez exercer ces droits ou si vous avez des questions concernant notre politique de confidentialité, veuillez nous contacter à l'adresseprivacy@example.com."
+        }
       </p>
       <p>
-        En utilisant l'application "Parole aux parents", vous acceptez notre politique de confidentialité. Si vous n'êtes pas d'accord avec cette
-        politique, veuillez ne pas utiliser l'application.
+        {
+          "En utilisant l'application 'Parole aux parents', vous acceptez notre politique de confidentialité. Si vous n'êtes pas d'accord avec cette politique, veuillez ne pas utiliser l'application."
+        }
       </p>
     </div>
   )
