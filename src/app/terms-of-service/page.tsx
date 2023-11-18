a73d6a39f75f1234e8fb72397265d07395f44a91
AlexTraveylan <monsieurdemares@gmail.com>
2023-11-18 22:04:25 +0100

    prvicy term of service

---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,37 @@
+export default async function TermsOfService() {
+  return (
+    <div>
+      <h1>Conditions d'utilisation</h1>
+      <p>Bienvenue sur l'application "Parole aux parents" !</p>
+      <p>En utilisant notre application, vous acceptez les conditions d'utilisation suivantes :</p>
+      <ol>
+        <li>Vous pouvez utiliser l'application pour poser des questions, faire des commentaires et les liker.</li>
+        <li>
+          Vous êtes responsable du contenu que vous publiez. Assurez-vous d'être respectueux envers les autres utilisateurs et de ne pas partager
+          de contenu inapproprié.
+        </li>
+        <li>Nous nous réservons le droit de supprimer tout contenu qui ne respecte pas nos règles de conduite.</li>
+        <li>
+          Vos commentaires et questions peuvent être visibles par d'autres utilisateurs. Assurez-vous de ne partager aucune information personnelle
+          sensible.
+        </li>
+        <li>
+          Vous pouvez liker et commenter le contenu d'autres utilisateurs, mais veuillez rester respectueux et bienveillant dans vos échanges.
+        </li>
+        <li>
+          Nous ne sommes pas responsables des informations partagées par les utilisateurs et nous ne pouvons pas garantir l'exactitude ou la
+          véracité de ces informations.
+        </li>
+        <li>Nous nous réservons le droit de suspendre ou de résilier votre compte si vous ne respectez pas nos conditions d'utilisation.</li>
+        <li>
+          Veuillez utiliser l'application de manière responsable et respectueuse. En cas de problème, veuillez nous contacter à l'adresse
+          support@example.com.
+        </li>
+      </ol>
+      <p>
+        En utilisant l'application "Parole aux parents", vous acceptez ces conditions d'utilisation. Si vous n'êtes pas d'accord avec ces
+        conditions, veuillez ne pas utiliser l'application.
+      </p>
+    </div>
+  )
+}
